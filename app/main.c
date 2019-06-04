ef9b367f177f972bccb9eb4231045c2e2d3ebd4d
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-06-04 22:30:22 +0200

    Added working resampling polyphase filter

--- app/main.c
+++ app/main.c
@@ -4,6 +4,7 @@
 
 #include "dac.h"
 #include "samplebank.h"
+#include "polyfilter.h"
 
 // Types ///////////////////////////////////////////////////////////////////////
 
@@ -15,6 +16,58 @@
 // Globals /////////////////////////////////////////////////////////////////////
 
 // Functions ///////////////////////////////////////////////////////////////////
+
+void resampler(int16_t *x, uint32_t length) 
+{
+    float y;
+    int16_t buffer[BUFFER_SIZE];
+    int32_t buffer_idx = 0;
+
+    int32_t m;
+    int32_t k;
+    int32_t n;
+    int32_t l;
+
+    uint32_t N = SUBFILTER_LENGTH;
+
+    const float *h = h4;
+    uint32_t h_length = (sizeof(h4)/sizeof(float));
+    uint32_t L = L4;
+    uint32_t M = M4;
+
+    for (m = 0; m < length; m++) {
+        buffer_idx = (m % BUFFER_SIZE);
+
+        n = (m*M) / L;
+        k = (m*M) % L;
+
+        if (n > length) {
+            fprintf(stderr, "Not enough input samples\n");
+            return;
+        }
+
+        y = 0;
+
+        for (l = 0; l < N; l++) {
+            // Skip unknown input samples
+            if (n - l < 0) {
+                continue;
+            }
+
+            // Poly-phase filter
+            y += L * h[l*L + k] * (float)x[n - l];
+        }
+
+        // Add the input sample to the output, to hear the interval
+        y = (y/2) + (x[m]/2);
+
+        buffer[buffer_idx] = (int16_t)y;
+
+        if (buffer_idx == BUFFER_SIZE-1) {
+            dac_write(buffer, BUFFER_SIZE);
+        }
+    }
+}
 
 // Interrupts //////////////////////////////////////////////////////////////////
 
@@ -48,19 +101,21 @@ int32_t main(void)
     x = samplebank[0].data;
     length = samplebank[0].length;
 
-    for (i = 0; i < length - BUFFER_SIZE; i += BUFFER_SIZE) {
-        // Fill buffer
-        for (n = 0; n < BUFFER_SIZE; n++) {
-            buffer[n] = x[i+n];
-        }
+    resampler(x, length);
 
-        // Output to DAC
-        err = dac_write(buffer, BUFFER_SIZE);
-        if (err != 0) {
-            fprintf(stderr, "Error during playback...\n");
-            goto clean_exit;
-        }
-    }
+    /* for (i = 0; i < length - BUFFER_SIZE; i += BUFFER_SIZE) { */
+    /*     // Fill buffer */
+    /*     for (n = 0; n < BUFFER_SIZE; n++) { */
+    /*         buffer[n] = x[i+n]; */
+    /*     } */
+    /*  */
+    /*     // Output to DAC */
+    /*     err = dac_write(buffer, BUFFER_SIZE); */
+    /*     if (err != 0) { */
+    /*         fprintf(stderr, "Error during playback...\n"); */
+    /*         goto clean_exit; */
+    /*     } */
+    /* } */
 
 clean_exit:
     // Close DAC
