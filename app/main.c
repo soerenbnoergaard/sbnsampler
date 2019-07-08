890fd6765f46c50808d58f40805ec4a4461f7dfe
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-07-08 17:05:25 +0200

    Increased dynamic range by using float in onepole

--- app/main.c
+++ app/main.c
@@ -31,7 +31,7 @@
 
 // Globals /////////////////////////////////////////////////////////////////////
 
-static preset_t *active_preset = &presets[0];
+static preset_t *active_preset = &presets[1];
 static settings_t global; // Settings loaded from the panel (or a preset)
 FILE *log_h;
 
--- app/vcf.c
+++ app/vcf.c
@@ -2,6 +2,8 @@
 #include "gpio.h"
 #include <stdio.h>
 
+#define VCF_ENABLE_RESONANCE 0
+
 static float map_midi_to_cutoff(uint8_t value)
 {
     // Map cutoff to VCF parameter `g`.
@@ -20,7 +22,7 @@ float map_midi_to_resonance(uint8_t value)
 
 int32_t vcf_onepole(int32_t x, float g, int32_t x1, int32_t y1)
 {
-    int32_t v = (13*(x>>4)) + (3*(x1>>4));
+    int32_t v = (int32_t)(0.8*(float)x + 0.2*(float)x1);
     return (v-y1)*g + y1;
 }
 
@@ -32,15 +34,23 @@ int16_t vcf_filter(int16_t input_sample, voice_t *voice, uint8_t cutoff, uint8_t
 
     // Short-hand names for VCF parameters
     int32_t n;
+    int32_t w[5]; // Current samples
     int32_t *w1 = voice->vcf.w1; // Delayed samples
-    int32_t w[5];
 
     float g = map_midi_to_cutoff(cutoff);
+
+#if VCF_ENABLE_RESONANCE
     float k = map_midi_to_resonance(resonance);
+#endif
 
     // Apply four one-pole filters
     x = input_sample;
+
+#if VCF_ENABLE_RESONANCE
     w[0] = x - ((int32_t)((float)w1[4]*k) - (x>>1)); // x>>1 = G_comp=0.5 from Valimaki
+#else
+    w[0] = x;
+#endif
     w[1] = vcf_onepole(w[0], g, w1[0], w1[1]);
     w[2] = vcf_onepole(w[1], g, w1[1], w1[2]);
     w[3] = vcf_onepole(w[2], g, w1[2], w1[3]);
@@ -52,6 +62,10 @@ int16_t vcf_filter(int16_t input_sample, voice_t *voice, uint8_t cutoff, uint8_t
     }
 
     // Return the output sample
+#if VCF_ENABLE_RESONANCE
     y = w[4] >> 4;
+#else
+    y = w[4];
+#endif
     return (int16_t)y;
 }
