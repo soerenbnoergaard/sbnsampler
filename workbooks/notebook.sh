383e871d9ed0882a28dafdb1a1abd220ce93b2fe
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-07-10 11:43:55 +0200

    Running jupyter from venv

---
+++ workbooks/notebook.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+PY="python3"
+PIP="pip3"
+VENV_DIR="venv"
+ACTIVATE="source $VENV_DIR/bin/activate"
+DEACTIVATE="deactivate"
+
+# Check arguments
+if [ ! -d $VENV_DIR ]; then
+    # Virtual environment not initialized
+    CMD="init"
+elif [ $# -eq 0 ]; then
+    # No arguments supplied
+    CMD="run"
+else
+    # Argument supplied
+    CMD="$1"
+fi
+
+# Clear the environment
+if [ $CMD == "reset" ]; then
+    rm -rf $VENV_DIR
+    CMD="init"
+fi
+
+# Initialize virtual environment
+if [ $CMD == "init" ]; then
+    $PY -m venv $VENV_DIR
+    $ACTIVATE
+    $PIP install jupyterlab numpy scipy matplotlib fortran-magic numba cython
+    $DEACTIVATE
+    CMD="run"
+fi
+
+# Run jupyter 
+if [ $CMD == "run" ]; then
+    $ACTIVATE
+    $PY -m jupyterlab
+    $DEACTIVATE
+fi
--- workbooks/start_jupyter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run natively
-python3 -m jupyterlab
-
-# Run the jupyter/datascience-notebook docker image.
-# docker run -it --rm -p 8888:8888 jupyter/datascience-notebook jupyter lab
-
