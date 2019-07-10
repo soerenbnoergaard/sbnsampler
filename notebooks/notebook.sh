d82a92f3eea369c6b27df914370f5835dcc29acb
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-07-10 15:03:36 +0200

    Designed a better voice mapping algorithm.

--- notebooks/notebook.sh
+++ notebooks/notebook.sh
@@ -28,7 +28,7 @@ fi
 if [ $CMD == "init" ]; then
     $PY -m venv $VENV_DIR
     $ACTIVATE
-    $PIP install jupyterlab numpy scipy matplotlib fortran-magic numba cython
+    $PIP install jupyter numpy scipy matplotlib fortran-magic numba cython
     $DEACTIVATE
     CMD="run"
 fi
@@ -36,6 +36,6 @@ fi
 # Run jupyter 
 if [ $CMD == "run" ]; then
     $ACTIVATE
-    $PY -m jupyterlab
+    $PY -m notebook --browser="chromium-browser"
     $DEACTIVATE
 fi
