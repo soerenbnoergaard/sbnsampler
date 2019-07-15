85a06ee1fdd57e40968e5ca5147b83cbf7d30a8b
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-07-15 18:52:41 +0200

    Think the simulator works.

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from sbnsampler import simulator as sim
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
 sim.init()
 sim.set_length(44100 * 5)
 sim.run()
 sim.close()
+
+y = np.fromfile("output.bin", dtype="<h")
+wavfile.write("output.wav", 44100, y)
