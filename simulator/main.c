eefdcac62f858e0706e685f71630cbc028be4241
Søren Bøgeskov Nørgaard <soerenbnoergaard@gmail.com>
2019-07-17 21:04:24 +0200

    Added simulator notebook.

--- simulator/main.c
+++ simulator/main.c
@@ -7,10 +7,6 @@
 #include "hardware.h"
 #include "process.h"
 
-#define MAX_NUM_RETRIES 512
-
-static int32_t simulation_length = 0;
-
 int32_t sim_init(void)
 {
     if (process_init("../app/sound") != 0) {
@@ -21,12 +17,6 @@ int32_t sim_init(void)
         return 1;
     }
 
-    return 0;
-}
-
-int32_t sim_set_length(int32_t length)
-{
-    simulation_length = length;
     return 0;
 }
 
@@ -44,51 +34,11 @@ int32_t sim_close(void)
     return 0;
 }
 
-int32_t sim_run(void)
+int32_t sim_tick(void)
 {
     int32_t err;
-    int32_t num_fails = 0;
-    int32_t num_ticks = 0;
-
-    do {
-        err = 0;
-        err += hardware_tick();
-        err += process_tick();
-
-        if (num_ticks++ > simulation_length) {
-            break;
-        }
-
-        if (err == 0) {
-            continue;
-        }
-
-        // Re-initialize hardware if the signal path fails
-        num_fails += 1;
-        if (hardware_close() != 0) {
-            return 1;
-        }
-        if (hardware_init() != 0) {
-            return 1;
-        }
-
-    } while (num_fails < MAX_NUM_RETRIES);
-    return 0;
-}
-
-int32_t main(void)
-{
-    if (sim_init() != 0) {
-        return 1;
-    }
-
-    if (sim_set_length(44100 * 5) != 0) {
-        return 1;
-    }
-
-    if (sim_run() != 0) {
-        return 1;
-    }
-
-    return 0;
+    err = 0;
+    err += hardware_tick();
+    err += process_tick();
+    return err;
 }
--- simulator/midi.c
+++ simulator/midi.c
@@ -1,19 +1,20 @@
 #include "midi.h"
 #include "midi_stanford.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 static int32_t sample_counter;
 static int32_t sim_midi_counter;
-static midi_message_t sim_midi_messages[128];
-static int32_t sim_midi_positions[128];
+static midi_message_t sim_midi_message;
+static bool sim_midi_available;
 
 // SIMULATION ONLY FUNCTION - EXPOSED THROUGH THE DLL!
-int32_t _midi_enqueue(int32_t index, int32_t position, uint8_t status, uint8_t data0, uint8_t data1)
+int32_t _midi_enqueue(uint8_t status, uint8_t data0, uint8_t data1)
 {
-    sim_midi_positions[index] = position;
-    sim_midi_messages[index].status = status;
-    sim_midi_messages[index].data[0] = data0;
-    sim_midi_messages[index].data[1] = data1;
+    sim_midi_message.status = status;
+    sim_midi_message.data[0] = data0;
+    sim_midi_message.data[1] = data1;
+    sim_midi_available = true;
     return 0;
 }
 
@@ -23,9 +24,6 @@ int32_t midi_init(void)
 
     sample_counter = 0;
     sim_midi_counter = 0;
-    for (n = 0; n < 128; n++) {
-        sim_midi_positions[n] = -1;
-    }
     return 0;
 }
 
@@ -40,16 +38,15 @@ int32_t midi_get(midi_message_t *m)
     int32_t ret;
 
     // Return midi messages at certain points in time
-    if (sample_counter == sim_midi_positions[sim_midi_counter]) {
-        *m = sim_midi_messages[sim_midi_counter];
-        sim_midi_counter += 1;
+    if (sim_midi_available) {
+        *m = sim_midi_message;
+        sim_midi_available = false;
         ret = 0;
     }
     else {
         ret = 1;
     }
 
-    sample_counter += 1;
     return ret;
 }
 
